9b4dc40640c09f189dddcea46357f492195cf73a
Sergei Andreev <s.andreev.it@gmail.com>
2020-09-30 19:49:53 +0500

    feat: complete deam-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let firstLattersOfMembers = [];
+
+  members.forEach((member) => {
+    if (typeof member === 'string') {
+      firstLattersOfMembers.push(member.trim().slice(0,1).toUpperCase());
+    }
+  });
+
+  return firstLattersOfMembers.sort().join('');
 };
