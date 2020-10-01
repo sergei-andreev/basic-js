978deb58d8685fb35d2c1b03e6366ae000e75d15
sergei-andreev <s.andreev.it@gmail.com>
2020-10-02 00:52:04 +0500

    feat: complete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error('Not array');
+
+  const resultArr = [];
+
+  const operations = [
+    '--discard-next',
+    '--discard-prev',
+    '--double-next',
+    '--double-prev'
+  ];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i += 1;
+        break;
+
+      case '--discard-prev':
+        if (resultArr.length !== 0
+          && arr[i - 2] !== '--discard-next') {
+          resultArr.pop();
+        }
+        break;
+
+      case '--double-next':
+        resultArr.push(arr[i + 1]);
+        break;
+
+      case '--double-prev':
+        if (i !== 0 && arr[i - 2] !== '--discard-next') {
+          resultArr.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        resultArr.push(arr[i]);
+        break;
+    }
+  }
+
+  return resultArr.filter((element) => element !== undefined);
 };
