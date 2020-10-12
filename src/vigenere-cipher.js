7d500c11aac6b7f078d520056f96359a20a1a0a3
Sergei Andreev <s.andreev.it@gmail.com>
2020-10-12 13:49:00 +0500

    feat: add vignere-chipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isForwardMode = true) {
+    this.isForwardMode = isForwardMode;
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  }
+
+  getString(inMessage, inKey, type) {
+    const message = inMessage.toLowerCase();
+    const key = inKey.toLowerCase();
+
+    if (!message || !key) throw new CustomError('Неполные данные');
+
+    const maxlength = message.length;
+    let result = '';
+    let c;
+  
+    for (let indexStr = 0, indexKey = 0; indexStr < maxlength; indexStr += 1) {
+      if (this.alphabet.indexOf(message[indexStr]) !== -1) {
+        const mi = this.alphabet.indexOf(message[(indexStr >= message.length) ? indexStr % message.length : indexStr]);
+        const ki_s = key[(indexKey >= key.length) ? (indexKey % key.length) : indexKey];
+        const ki = this.alphabet.indexOf(ki_s);
+        c = this.alphabet[(this.alphabet.length + (type === 'encrypt' ? mi + ki : mi - ki)) % this.alphabet.length];
+        indexKey += 1;
+      } else {
+        c = message[indexStr];
+      }
+  
+      result += c.toUpperCase();
+    }
+  
+    if (!this.isForwardMode) {
+      return result.split('').reverse().join('');
+    }
+    return result;
+  }
+  
+  encrypt(inMessage, inKey) {
+    return this.getString(inMessage, inKey, 'encrypt');
+  } 
+
+  decrypt(inMessage, inKey) {
+    return this.getString(inMessage, inKey, 'decrypt');
   }
 }
 
